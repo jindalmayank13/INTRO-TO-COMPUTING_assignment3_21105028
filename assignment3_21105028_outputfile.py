ec8801b5809b6ace2a64418eaf8fd93df3b43218
Mayank Jindal <93427749+jindalmayank13@users.noreply.github.com>
2022-02-05 14:01:23 +0530

    Add files via upload

---
+++ assignment3_21105028_outputfile.py
@@ -0,0 +1,108 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: E:/assignment3_21105028_source code.py ===============
+
+Q.1
+Enter the string:Hello my name is Mayank and my branch is ECE
+
+Dictionary containing {'Word':'Word Count'} is shown below:
+{'hello': 1, 'is': 2, 'branch': 1, 'mayank': 1, 'and': 1, 'my': 2, 'name': 1, 'ece': 1}
+
+Q.2
+Enter Date below to get date of next day.
+
+Enter Day [1-31]:30
+Enter Month [1-12]:12
+Enter Year [1800-2025]:2000
+
+Next Date in format Day/Month/Year is: 1/1/2001
+
+Q.3
+Enter the numbers separated by space:1 2 3 4 5 6 7 8 9
+
+List containing (n,n^2) is shown below:
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81)]
+
+Q.4
+Enter grade points:10
+Your grade is 'A+' and Outstanding performance.
+
+Q.5
+The pattern is printed below:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+Q.6
+Enter student name:Mayank
+Enter student SID:21105028
+Enter Y to continue or N to end:y
+Enter student name:Himanshu
+Enter student SID:21105029
+Enter Y to continue or N to end:y
+Enter student name:Ayush
+Enter student SID:21105049
+Enter Y to continue or N to end:y
+Enter student name:Soumil
+Enter student SID:21105005
+Enter Y to continue or N to end:n
+
+Q.6(a)
+The Dictionary containing {'SID':'Name'} is shown below
+{21105028: 'Mayank', 21105029: 'Himanshu', 21105049: 'Ayush', 21105005: 'Soumil'}
+
+Q.6(b)
+The Dictionary after sorting according to name:
+{21105049: 'Ayush', 21105029: 'Himanshu', 21105028: 'Mayank', 21105005: 'Soumil'}
+
+Q.6(c)
+The Dictionary after sorting according to SID:
+{21105005: 'Soumil', 21105028: 'Mayank', 21105029: 'Himanshu', 21105049: 'Ayush'}
+
+Q.6(d)
+Enter SID to get name of student:21105028
+Name of student with SID 21105028 is Mayank.
+
+Q.7
+Enter number of elements N in fibonacci series:
+[N must be positive Integer]: N=15
+
+The fibonacci series with 15 elements is shown below:
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+
+Average of given fibonacci series is 106.40
+
+Q.8
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+Q.8(a)
+
+A new Set of all 'elements that are in Set1 and Set2 but not both' is:
+{1, 3, 5, 6, 8}
+
+Q.8(b)
+
+A new set of all elements that are 'in only one of the three sets Set1,Set2 and Set3' is:
+{3, 6, 8, 9, 13, 17}
+
+Q.8(c)
+
+A new set of elements that are 'exactly in two of the sets Set1, Set2 and Set3' is:
+{1, 2, 4, 5}
+
+Q.8(d)
+
+A new set of all Integers in the 'range 1 to 10' that are 'not in Set1' is:
+{6, 7, 8, 9, 10}
+
+Q.8(e)
+
+A new set of all Integers in the range 1 to 10 that are not in Set1,Set2 and Set3.
+{10, 7}
